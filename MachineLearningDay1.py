df7fd395ec93f3e096c6dae20b44361fbf394b3a
ram2010mechatronics <ram2010mechatronics@gmail.com>
2018-07-12 00:29:11 +0530

    Initial

---
+++ MachineLearningDay1.py
@@ -0,0 +1,7 @@
+# Import the pandas library.
+import pandas
+
+# Read in the data.
+games = pandas.read_csv("R:\Python_learning\Datas.csv")
+# Print the names of the columns in games.
+print(games.columns)
